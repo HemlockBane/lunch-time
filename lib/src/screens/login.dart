97dd306696c6808f9d8f3a93a7b06a2ddc2d1f6f
Obinna Igwe <igwenus619@gmail.com>
2019-08-24 21:18:42 +0100

    Add image and text to login screen

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../constants/app_images.dart';
+import '../widgets/screen_data.dart';
 
 class LoginScreen extends StatefulWidget {
 
@@ -11,9 +14,25 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    ScreenData().init(context);
     return Scaffold(
       body: SingleChildScrollView(
-        
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: ScreenData.blockSizeHorizontal * 4.166),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: ScreenData.screenHeight * 0.15625,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(AppImages.clock, width: ScreenData.screenWidth * 0.5556, height: ScreenData.screenHeight * 0.3125),
+                ],
+              ),
+              Text('Lunch Time'),
+              Text('Ready for munchies?')
+            ],
+          ),
+        )
       ),
     );
   }
